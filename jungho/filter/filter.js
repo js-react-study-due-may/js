6880b41e693466a8f9aa4aaa0f99327b077b5318
joshyeom <jhyeom95@daum.net>
2025-04-13 22:25:03 +0900

    0413 filter

---
+++ jungho/filter/filter.js
@@ -0,0 +1,17 @@
+Array.prototype.myFilter = function(condition){
+    const arr = []
+
+    for(let i = 0 ; i < this.length;  i++){
+        const result = condition(this[i], i, this);
+        if(result){
+            arr.push(this[i])
+        }
+    }
+    return arr
+}
+
+const arr = [1,2,3];
+
+const test = arr.myFilter((v) => v > 1);
+
+console.log(test);
